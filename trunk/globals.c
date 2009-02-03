6d8935094c1b96a7cce54bad53f9b2cd40f80e65
kai.uwe.hermann <kai.uwe.hermann@f616791a-b1b0-11dd-91e6-f3ebeb48568e>
2009-02-03 20:25:15 +0000

    add global vars

---
+++ trunk/globals.c
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * globals.c : global vars
+ *****************************************************************************
+ * Copyright (C) 2009 the tg13-4-robotic-project Team
+ *
+ * Authors: Kai Hermann <kai -dot- uwe -dot- hermann -at- googlemail -dot- com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+unsigned int msl;
+unsigned int msr;
+unsigned int black_l;
+unsigned int black_r;
+unsigned int white_l;
+unsigned int white_r;
